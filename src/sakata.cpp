429fef931693951044ddc205dc1f6373d0ce83d4
Petuina <178623265@qq.com>
2025-10-26 22:55:14 +0800

    init commit.

---
+++ src/sakata.cpp
@@ -0,0 +1,6 @@
+#include "sakata.hpp"
+
+namespace Sakata
+{
+
+}
---
+++ src/sakata.hpp
@@ -0,0 +1,95 @@
+#include <cstdint>
+#include <cstddef>
+#include <string>
+#include <span>
+
+namespace Sakata
+{
+
+// Constant values.
+    
+constexpr uint8_t PACKET_START       = 0xAA;
+constexpr uint8_t PACKET_END         = 0x55;
+constexpr size_t  MAX_PAYLOAD      = 256;  // 最大有效载荷
+constexpr size_t  MAX_BUFFERSIZE   = 1024;
+
+enum class Commands : uint8_t {
+    REQUEST,
+    RESPONSE,
+    RETRY,
+    BUSY,
+    FREE
+};
+
+// Basic data structs.
+
+using FunctionID = int32_t;
+using MemoryReference = std::span<uint8_t>;
+
+#pragma pack(push, 1)
+struct PacketHeader {
+    uint8_t     start;        // 起始标志
+    Commands    cmd;          // 命令类型
+    int16_t    frameId;       // 帧序号（允许溢出归零）
+    int16_t    dataLength;    // 数据长度
+};
+#pragma pack(pop)
+
+// The length of data members is dynamic and needs to be manually initialized before use。
+struct PacketBody
+{
+    // Function and parameters
+    FunctionID functionId{-1};
+    MemoryReference functionParameter;
+    
+    uint8_t end;
+    // XOR for whole packet, reserved now.
+    //uint8_t checksum{0};   
+};
+
+class Packet
+{
+    private:
+    PacketHeader* header;
+    PacketBody body;
+    bool valid{false};
+
+    public:
+    const PacketHeader& getHeader();
+    const PacketBody& getBody();
+    const bool isValid() {return valid;}
+
+    bool buildFromRawData(const MemoryReference& data);
+    Packet(const MemoryReference& data) {buildFromRawData(data);}
+    Packet() = default;
+};
+
+
+struct NodeInfo
+{
+    const std::string nodeName;
+};
+
+
+class SakataNode
+{
+    // Node information
+    private:
+    struct NodeInfo {
+        const std::string nodeName;
+    }nodeinfo;
+
+    // Peer control and APIs.
+    private:
+
+    public:
+
+    // Function management and APIs.
+    private:
+
+    public:
+
+};
+
+
+} // Namespace Sakata
