1ad6a5753dae2a00c1a1e72efd0a08c2ccbd9950
Petuina <178623265@qq.com>
2025-10-29 14:51:10 +0000

    commit

--- src/sakata.hpp
+++ src/sakata.hpp
@@ -2,15 +2,16 @@
 #include <cstddef>
 #include <string>
 #include <span>
+#include <functional>
 
 namespace Sakata
 {
 
 // Constant values.
     
-constexpr uint8_t PACKET_START       = 0xAA;
-constexpr uint8_t PACKET_END         = 0x55;
-constexpr size_t  MAX_PAYLOAD      = 256;  // 最大有效载荷
+constexpr uint8_t PACKET_START     = 0xAA;
+constexpr uint8_t PACKET_END       = 0x55;
+constexpr size_t  MAX_PAYLOAD      = 256;
 constexpr size_t  MAX_BUFFERSIZE   = 1024;
 
 enum class Commands : uint8_t {
@@ -21,21 +22,23 @@ enum class Commands : uint8_t {
     FREE
 };
 
-// Basic data structs.
+// Basic data structs & types.
 
 using FunctionID = int32_t;
 using MemoryReference = std::span<uint8_t>;
+using MemoryReferenceHandler = std::function<bool(MemoryReference)>;
 
 #pragma pack(push, 1)
 struct PacketHeader {
-    uint8_t     start;        // 起始标志
-    Commands    cmd;          // 命令类型
-    int16_t    frameId;       // 帧序号（允许溢出归零）
-    int16_t    dataLength;    // 数据长度
+    uint8_t    start;
+    Commands   cmd;
+    int16_t    frameId;
+    int16_t    dataLength;
 };
 #pragma pack(pop)
 
-// The length of data members is dynamic and needs to be manually initialized before use。
+
+// The length of data members is dynamic and needs to be manually initialized before use
 struct PacketBody
 {
     // Function and parameters
@@ -46,6 +49,7 @@ struct PacketBody
     // XOR for whole packet, reserved now.
     //uint8_t checksum{0};   
 };
+
 
 class Packet
 {
@@ -62,6 +66,18 @@ class Packet
     bool buildFromRawData(const MemoryReference& data);
     Packet(const MemoryReference& data) {buildFromRawData(data);}
     Packet() = default;
+};
+
+
+class PointToPointConnection {
+    public:
+        inline const bool isActive() { return active; }
+        inline void initailze(MemoryReferenceHandler handler) { if(handler) {on_send = handler; active = true;} }
+        PointToPointConnection() = default;
+        PointToPointConnection(MemoryReferenceHandler handler):on_send(handler), active(true) {}
+    private:
+        bool active{false};
+        MemoryReferenceHandler on_send;
 };
 
 
