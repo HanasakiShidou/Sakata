b166f030ba313342389bdf3b006c399cbbf6985f
Petuina <178623265@qq.com>
2026-01-11 17:17:11 +0000

    move request queue from local node to remote node.

---
+++ src/packet.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <vector>
+#include <span>
+#include <functional>
+
+namespace Sakata
+{
+// Constant values.
+    
+constexpr uint8_t PACKET_START     = 0xAA;
+constexpr uint8_t PACKET_END       = 0x55;
+constexpr size_t  MAX_PAYLOAD      = 256;
+constexpr size_t  MAX_BUFFERSIZE   = 1024;
+constexpr int CALL_TIME_OUT_TIME   = 5; // in second
+
+enum class Commands : uint8_t {
+    REQUEST,
+    RESPONSE,
+    RETRY,
+    BUSY,
+    FREE
+};
+
+// Basic data structs & types.
+
+using FunctionID = int32_t;
+using NodeID = int32_t;
+using RequestSequenceNumber = int32_t;
+using RawData = std::vector<uint8_t>;
+using RawDataHandler = std::function<bool(RawData)>;
+using RawDataView = std::span<const uint8_t>;
+
+
+/*
+* PACKET STRUCTURE
+* [] -> Necessary fields.
+* <> -> Optional fields.
+*
+* Current packet structure:
+* [START 1byte] [COMMANDS 1btye] [SEQUENCE 2byte] [LENGTH 2byte]
+* <FUNCTION_ID 4byte>
+* <REQUEST_SEQ_NUM 4byte>
+* <FUNCTION_RESULT dynamic length>
+* <FUNCTION_PARAMETER dynamic length>
+* [END 1byte]
+*/
+
+struct PacketHeader {
+    uint8_t    start;
+    Commands   cmd;
+    int16_t    sequence;
+    int16_t    dataLength;
+};
+
+// The length of data members is dynamic and needs to be manually initialized before use
+struct PacketBody
+{
+    // Optional fields
+    // Function and parameters
+    FunctionID functionId{-1};
+    RequestSequenceNumber requestSN{0};
+    RawData functionParameter;
+    RawData functionResult;
+    
+    // XOR for whole packet, reserved now.
+    //uint8_t checksum{0};   
+    uint8_t end;
+};
+
+
+class Packet : public PacketHeader, public PacketBody
+{
+    //private: -> TODO: for test
+    public:
+    bool valid{false};
+
+    public:
+    const PacketHeader& getHeader() { return *this; }
+    const PacketBody& getBody() { return *this; }
+    const bool isValid() { return valid; }
+
+    static Packet deSerialize(const RawData& data);
+    const RawData serialize();
+    Packet(const RawData& data) { deSerialize(data); }
+    Packet() = default;
+};
+
+} // Namespace Sakata
--- src/sakata.hpp
+++ src/sakata.hpp
@@ -1,91 +1,15 @@
+#include "packet.hpp"
 #include <cstdint>
 #include <cstddef>
 #include <string>
 #include <span>
+#include <atomic>
 #include <functional>
 #include <unordered_map>
 #include <memory>
 
 namespace Sakata
 {
-
-// Constant values.
-    
-constexpr uint8_t PACKET_START     = 0xAA;
-constexpr uint8_t PACKET_END       = 0x55;
-constexpr size_t  MAX_PAYLOAD      = 256;
-constexpr size_t  MAX_BUFFERSIZE   = 1024;
-constexpr int CALL_TIME_OUT_TIME   = 5; // in second
-
-enum class Commands : uint8_t {
-    REQUEST,
-    RESPONSE,
-    RETRY,
-    BUSY,
-    FREE
-};
-
-// Basic data structs & types.
-
-using FunctionID = int32_t;
-using NodeID = int32_t;
-using RequestSequenceNumber = int32_t;
-using RawData = std::vector<uint8_t>;
-using RawDataHandler = std::function<bool(RawData)>;
-
-/*
-* PACKET STRUCTURE
-* [] -> Necessary fields.
-* <> -> Optional fields.
-*
-* Current packet structure:
-* [START 1byte] [COMMANDS 1btye] [SEQUENCE 2byte] [LENGTH 2byte]
-* <FUNCTION_ID 4byte>
-* <REQUEST_SEQ_NUM 4byte>
-* <FUNCTION_RESULT dynamic length>
-* <FUNCTION_PARAMETER dynamic length>
-* [END 1byte]
-*/
-
-struct PacketHeader {
-    uint8_t    start;
-    Commands   cmd;
-    int16_t    sequence;
-    int16_t    dataLength;
-};
-
-// The length of data members is dynamic and needs to be manually initialized before use
-struct PacketBody
-{
-    // Optional fields
-    // Function and parameters
-    FunctionID functionId{-1};
-    RequestSequenceNumber requestSN{0};
-    RawData functionParameter;
-    RawData functionResult;
-    
-    // XOR for whole packet, reserved now.
-    //uint8_t checksum{0};   
-    uint8_t end;
-};
-
-
-class Packet : public PacketHeader, public PacketBody
-{
-    //private: -> TODO: for test
-    public:
-    bool valid{false};
-
-    public:
-    const PacketHeader& getHeader() { return *this; }
-    const PacketBody& getBody() { return *this; }
-    const bool isValid() { return valid; }
-
-    static Packet deSerialize(const RawData& data);
-    const RawData serialize();
-    Packet(const RawData& data) { deSerialize(data); }
-    Packet() = default;
-};
 
 // Raw data transfer layer.
 class PointToPointConnection {
@@ -204,12 +128,42 @@ struct SakataRequest {
     } status {OUTCOMING_UNINIALIZED};
     const RequestSequenceNumber requestSN{0};
     const FunctionInfo& func;
-    RawData incomingBuffer{};
-    RawData outcomingPayload{};
+    // Function call parameter buffer
+    RawData requestParameter{};
+    // Responsed function call result.
+    RawData responseResult{};
+    
+    const Packet buildResp();
+
+    private:
+    // For both income/outcome request.
+    // Only if the request is done, we can start to transfer data.
+    std::atomic<bool> _finished{false};
+
+    public:
+    // The node will only store "true" and load "false"
+    // The user application will only store "false" and load "true"
+    void inline release(const bool val) {
+        _finished.store(val, std::memory_order_release);
+    }
+    bool inline acquire() {
+        return _finished.load(std::memory_order_acquire);
+    }
 };
 
-struct SakataRemoteRequest : public SakataRequest {
-    NodeID remoteNodeId{0};
+class SakataCallResult {
+    SakataRequest& request;
+    public:
+    inline bool isDone() {
+        return request.acquire() == true;
+    }
+    // template
+    void read();
+    // When result read is done, the node can release the resource.
+    ~SakataCallResult() {
+        request.status = SakataRequest::REQ_FINISHED;
+        request.release(true);
+    }
 };
 
 struct BaseNode
@@ -224,7 +178,8 @@ class RemoteNode : public BaseNode
 {
     private:
     PointToPointConnection connection;
-    std::vector<SakataRequest> outcomingRequest;
+    std::vector<SakataRequest> outcomingCallRequest;
+    std::vector<SakataRequest> incomingCallRequest;
     SakataNode* parentNode;
 
     // true means a request is sent.
@@ -257,7 +212,7 @@ class RemoteNode : public BaseNode
     friend class SakataNode;
 };
 
-// Complex node 
+// Local node 
 class SakataNode : public BaseNode
 {
     // Peer control and APIs.
